c542e6b854099eb25f7f66e4f1a1555b69ef29d0
lsc58461 <lsc58461@naver.com>
2024-05-15 17:35:01 +0900

    feat: 비밀번호 암호화 & 에러 핸들링

--- src/controllers/vote.controller.js
+++ src/controllers/vote.controller.js
@@ -1,78 +1,165 @@
+const bcrypt = require("bcryptjs");
+
 const db = require("../models");
 
 const { Vote, Content, Count } = db;
 
 const getVote = async (req, res) => {
-  const { id } = req.params;
-  const vote = await Vote.findOne({ where: { id } });
-  const contents = await Content.findAll({
-    where: { voteId: id },
-    attributes: ["id", "voteId", "content", "createdAt", "updatedAt"],
-  });
-  const result = { ...vote.dataValues, contents };
+  try {
+    const { id } = req.params;
+    const vote = await Vote.findOne({
+      where: { id },
+      attributes: {
+        exclude: ["password"],
+      },
+    });
 
-  if (vote) {
-    res.send(result);
-    return;
+    if (!vote) {
+      res.status(404).send({ message: "투표가 존재하지 않습니다." });
+      return;
+    }
+
+    const contents = await Content.findAll({
+      where: { voteId: id },
+      attributes: ["id", "voteId", "content", "createdAt", "updatedAt"],
+    });
+
+    const selectedCounts = contents.map(async (content) => {
+      const count = await Count.findAll({
+        where: { voteId: id, contentId: content.dataValues.id },
+        attributes: ["id", "voteId", "contentId"],
+      });
+
+      return count.length;
+    });
+
+    const result = await Promise.all(selectedCounts).then((counts) => {
+      const participantCounts = counts.reduce((acc, cur) => acc + cur, 0);
+      return {
+        ...vote.dataValues,
+        participantCounts,
+        contents: contents.map((content, index) => ({
+          ...content.dataValues,
+          selectedCounts: counts[index],
+        })),
+      };
+    });
+
+    if (result) {
+      res.send(result);
+      return;
+    }
+  } catch {
+    res.status(500).send({ message: "투표 조회에 실패했습니다." });
   }
-
-  res.status(404).send({ message: "투표가 존재하지 않습니다." });
 };
 
 const createVote = async (req, res) => {
-  const newVote = req.body;
-  console.log(newVote.contents);
-  const vote = await Vote.create(newVote);
-  const contents = await Content.bulkCreate(
-    newVote.contents.map((content) => ({
-      content,
-      voteId: vote.id,
-    }))
-  );
+  try {
+    const newVote = req.body;
 
-  if (vote) {
-    res.send({ ...vote.dataValues, contents });
-    return;
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(newVote.password, salt);
+
+    const vote = await Vote.create({ ...newVote, password: hash });
+
+    const contents = await Content.bulkCreate(
+      newVote.contents.map((content) => ({
+        content,
+        voteId: vote.id,
+      }))
+    );
+
+    if (vote) {
+      res.status(201).send({ ...vote.dataValues, contents });
+      return;
+    }
+  } catch {
+    res.status(500).send({ message: "투표 생성에 실패했습니다." });
   }
-
-  res.status(404).send({ message: "투표 생성 실패~" });
 };
 
 const editVote = async (req, res) => {
-  const { id } = req.params;
-  const newInfo = req.body;
-  const result = await Vote.update(newInfo, { where: { id } });
+  try {
+    const { id } = req.params;
+    const newInfo = req.body;
 
-  if (result[0]) {
-    res.send({ message: `${result[0]}개의 행이 영향받았습니다.` });
-    return;
+    const vote = await Vote.findOne({ where: { id } });
+
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      vote.password
+    );
+
+    if (!isPasswordCorrect) {
+      res.status(401).send({ message: "비밀번호가 틀렸습니다." });
+      return;
+    }
+
+    const result = await Vote.update(
+      { periodEnd: newInfo.periodEnd },
+      { where: { id } }
+    );
+
+    if (result[0]) {
+      res.send({ message: `${result[0]}개의 행이 영향받았습니다.` });
+      return;
+    }
+  } catch {
+    res.status(500).send({ message: "투표 수정에 실패했습니다." });
   }
-
-  res.status(404).send({ message: "수정실패~" });
 };
 
 const doVote = async (req, res) => {
-  const { voteId, contentId } = req.params;
-  const doVote = await Count.create({ voteId: voteId, contentId: contentId });
+  try {
+    const { voteId, contentId } = req.params;
+    const validateVote = await Vote.findOne({ where: { id: voteId } });
 
-  if (doVote) {
-    res.send({ message: "투표가 완료되었습니다." });
-    return;
+    if (!validateVote) {
+      res.status(404).send({ message: "존재하지 않는 투표입니다." });
+      return;
+    }
+
+    const doVote = await Count.create({ voteId: voteId, contentId: contentId });
+
+    if (doVote) {
+      res.send({ message: "투표가 완료되었습니다." });
+      return;
+    }
+  } catch {
+    res.status(500).send({ message: "투표를 실패했습니다." });
   }
-
-  res.status(404).send({ message: "투표 실패~" });
 };
 
 const deleteVote = async (req, res) => {
-  const { id } = req.params;
-  const deletedCount = await Vote.destroy({ where: { id } });
+  try {
+    const { id } = req.params;
+    const vote = await Vote.findOne({ where: { id } });
 
-  if (deletedCount) {
-    res.send({ message: `${deletedCount}개 삭제 되었습니다.` });
-    return;
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      vote.password
+    );
+
+    if (!isPasswordCorrect) {
+      res.status(401).send({ message: "비밀번호가 틀렸습니다." });
+      return;
+    }
+
+    const deletedContents = await Content.destroy({ where: { voteId: id } });
+
+    const deletedCounts = await Count.destroy({ where: { voteId: id } });
+
+    const deletedVote = await Vote.destroy({ where: { id } });
+
+    if (deletedVote) {
+      res.send({
+        message: `Vote: ${deletedVote}개, Counts: ${deletedCounts}개, Contents: ${deletedContents}개 삭제 되었습니다.`,
+      });
+    }
+  } catch {
+    res.status(500).send({ message: "투표 삭제를 실패했습니다." });
   }
-
-  res.status(404).send({ message: "삭제할 수 없습니다." });
 };
 
 module.exports = {
--- src/controllers/votelist.controller.js
+++ src/controllers/votelist.controller.js
@@ -4,37 +4,45 @@ const { convertToKoreanTime } = require("../utils/convertToKoreanTime");
 const { Vote, Count, Content } = db;
 
 const getVoteList = async (req, res) => {
-  const { offset, limit } = req.query;
-  const lists = await Vote.findAll();
+  try {
+    const { offset, limit } = req.query;
+    const lists = await Vote.findAll({
+      attributes: {
+        exclude: ["password"],
+      },
+    });
 
-  const participantCounts = lists.map(async (vote) => {
-    const count = await Count.findAll({ where: { voteId: vote.id } });
-    return count.length;
-  });
+    const participantCounts = lists.map(async (vote) => {
+      const count = await Count.findAll({ where: { voteId: vote.id } });
+      return count.length;
+    });
 
-  const contents = await Content.findAll();
-  console.log(contents);
-  const result = await Promise.all(participantCounts).then((counts) =>
-    lists.map((vote, index) => {
-      const convertTime = convertToKoreanTime(new Date());
+    const contents = await Content.findAll();
 
-      return {
-        ...vote.dataValues,
-        contents: contents
-          .filter((content) => content.voteId === vote.id)
-          .map((content) => content.content),
-        participantCounts: counts[index],
-        isClosed: vote.dataValues.periodEnd < convertTime,
-      };
-    })
-  );
+    const result = await Promise.all(participantCounts).then((counts) =>
+      lists.map((vote, index) => {
+        const convertTime = convertToKoreanTime(new Date());
 
-  if (offset && limit) {
-    res.send(result.slice(offset - 1, offset - 1 + limit));
-    return;
+        return {
+          ...vote.dataValues,
+          contents: contents
+            .filter((content) => content.voteId === vote.id)
+            .map((content) => content.content),
+          participantCounts: counts[index],
+          isClosed: vote.dataValues.periodEnd < convertTime,
+        };
+      })
+    );
+
+    if (offset && limit) {
+      res.send(result.slice(offset - 1, offset - 1 + limit));
+      return;
+    }
+
+    res.send(result);
+  } catch {
+    res.status(500).send({ message: "투표 리스트 조회에 실패했습니다." });
   }
-
-  res.send(result);
 };
 
 module.exports = {
