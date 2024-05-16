1f26cec501dd3b5a52efd441cf6e00d7b9746415
lsc58461 <lsc58461@naver.com>
2024-05-16 17:59:51 +0900

    feat: total count 추가

--- src/controllers/votelist.controller.js
+++ src/controllers/votelist.controller.js
@@ -8,13 +8,8 @@ const getVoteList = async (req, res) => {
   try {
     const { offset, limit, search } = req.query;
 
-    if (offset <= 0 || limit <= 0) {
+    if (Number(offset) <= 0 || Number(limit) <= 0) {
       res.status(400).send({ message: "offset과 limit은 0보다 커야 합니다." });
-      return;
-    }
-
-    if (offset && limit && offset > limit) {
-      res.status(400).send({ message: "offset은 limit보다 작아야 합니다." });
       return;
     }
 
@@ -28,11 +23,17 @@ const getVoteList = async (req, res) => {
       };
     }
 
+    const totalCount = await Vote.count({
+      where: voteQuery,
+    });
+
     const lists = await Vote.findAll({
       where: voteQuery,
       attributes: {
         exclude: ["password"],
       },
+      offset: offset ? Number(offset) - 1 : 0,
+      limit: limit ? Number(limit) : 100,
     });
 
     const participantCounts = lists.map(async (vote) => {
@@ -57,12 +58,10 @@ const getVoteList = async (req, res) => {
       })
     );
 
-    if (offset && limit) {
-      res.send(result.slice(offset - 1, offset - 1 + limit));
-      return;
-    }
-
-    res.send(result);
+    res.send({
+      total: totalCount,
+      votes: result,
+    });
   } catch {
     res.status(500).send({ message: "투표 리스트 조회에 실패했습니다." });
   }
